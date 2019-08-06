7d5e44eb5bb0aec1688bcea2659b88e88566487e
Tyler Scott <tyduptyler13@gmail.com>
2019-08-06 15:13:12 -0600

    Autoformat

--- docker/build-test.sh
+++ docker/build-test.sh
@@ -2,9 +2,9 @@
 
 VERSION=3.24.2
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
-cd "$DIR" || ( echo "Failed to change to the script directory" && exit 1)
+cd "$DIR" || (echo "Failed to change to the script directory" && exit 1)
 
 cp "$HOME/dev/inversoft/cleanspeak/cleanspeak-management-interface/build/bundles/cleanspeak-management-interface_$VERSION-1_all.deb" cleanspeak-test/cleanspeak-management-interface
 cp "$HOME/dev/inversoft/cleanspeak/cleanspeak-webservice/build/bundles/cleanspeak-webservice_$VERSION-1_all.deb" cleanspeak-test/cleanspeak-webservice
