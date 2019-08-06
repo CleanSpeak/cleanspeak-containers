ca3f3190fab726e961c1ae0c0543e1fb2e964606
Tyler Scott <tyduptyler13@gmail.com>
2019-08-06 15:11:10 -0600

    Added .env, test images, and added the ability for silent mode to work

---
+++ docker/build-test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+VERSION=3.24.2
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+cd "$DIR" || ( echo "Failed to change to the script directory" && exit 1)
+
+cp "$HOME/dev/inversoft/cleanspeak/cleanspeak-management-interface/build/bundles/cleanspeak-management-interface_$VERSION-1_all.deb" cleanspeak-test/cleanspeak-management-interface
+cp "$HOME/dev/inversoft/cleanspeak/cleanspeak-webservice/build/bundles/cleanspeak-webservice_$VERSION-1_all.deb" cleanspeak-test/cleanspeak-webservice
+
+docker build -t cleanspeak/cleanspeak-management-interface:dev ./cleanspeak-test/cleanspeak-management-interface
+docker build -t cleanspeak/cleanspeak-webservice:dev ./cleanspeak-test/cleanspeak-webservice
