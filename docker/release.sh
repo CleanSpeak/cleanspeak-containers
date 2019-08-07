16c6daec3cf7d84278e945f81348787340ff04e0
Tyler Scott <tyduptyler13@gmail.com>
2019-08-07 15:21:50 -0600

    Versionify the dockerfiles and release script

--- docker/release.sh
+++ docker/release.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-VERSION=3.24.1
+if [ -z "$VERSION" ]; then
+  echo "VERSION is required!"
+  exit 1
+fi
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
-cd "$DIR" || ( echo "Failed to change to the script directory" && exit 1)
+cd "$DIR" || (echo "Failed to change to the script directory" && exit 1)
 
-docker build -t cleanspeak/cleanspeak-management-interface:$VERSION ./cleanspeak/cleanspeak-management-interface
-docker build -t cleanspeak/cleanspeak-management-interface:latest ./cleanspeak/cleanspeak-management-interface
-docker build -t cleanspeak/cleanspeak-webservice:$VERSION ./cleanspeak/cleanspeak-webservice
-docker build -t cleanspeak/cleanspeak-webservice:latest ./cleanspeak/cleanspeak-webservice
+docker build --build-arg CLEANSPEAK_VERSION=$VERSION -t "cleanspeak/cleanspeak-management-interface:$VERSION" ./cleanspeak/cleanspeak-management-interface
+docker build --build-arg CLEANSPEAK_VERSION=$VERSION -t "cleanspeak/cleanspeak-management-interface:latest" ./cleanspeak/cleanspeak-management-interface
+docker build --build-arg CLEANSPEAK_VERSION=$VERSION -t "cleanspeak/cleanspeak-webservice:$VERSION" ./cleanspeak/cleanspeak-webservice
+docker build --build-arg CLEANSPEAK_VERSION=$VERSION -t "cleanspeak/cleanspeak-webservice:latest" ./cleanspeak/cleanspeak-webservice
 
-docker push cleanspeak/cleanspeak-management-interface:$VERSION
-docker push cleanspeak/cleanspeak-management-interface:latest
-docker push cleanspeak/cleanspeak-webservice:$VERSION
-docker push cleanspeak/cleanspeak-webservice:latest
+docker push "cleanspeak/cleanspeak-management-interface:$VERSION"
+docker push "cleanspeak/cleanspeak-management-interface:latest"
+docker push "cleanspeak/cleanspeak-webservice:$VERSION"
+docker push "cleanspeak/cleanspeak-webservice:latest"
